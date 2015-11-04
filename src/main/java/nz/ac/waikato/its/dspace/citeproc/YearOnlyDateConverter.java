33bb2116fe37cb3c9b0b8756c4c16a00e85c264d
Andrea Schweer <schweer@waikato.ac.nz>
2015-11-05 09:01:56 +1300

    |Improve citeproc crosswalking for dates
    - offer year-only variant
    - don't hard code the destination field, use the supplied one

---
+++ src/main/java/nz/ac/waikato/its/dspace/citeproc/YearOnlyDateConverter.java
@@ -0,0 +1,11 @@
+package nz.ac.waikato.its.dspace.citeproc;
+
+/**
+ * @author Andrea Schweer schweer@waikato.ac.nz
+ *         for the University of Waikato's Institutional Research Repositories
+ */
+public class YearOnlyDateConverter extends DateConverter {
+    public YearOnlyDateConverter() {
+        fullDate = false;
+    }
+}
