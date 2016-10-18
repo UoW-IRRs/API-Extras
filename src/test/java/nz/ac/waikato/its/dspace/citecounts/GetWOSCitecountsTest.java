4f8c93fc26470a7c387fa8b750d59bd63f923898
Andrea Schweer <schweer@waikato.ac.nz>
2016-10-18 16:03:29 +1300

    |Fix tests to make them independent of actual citecounts for the test items - only check structure of the response

--- src/test/java/nz/ac/waikato/its/dspace/citecounts/GetWOSCitecountsTest.java
+++ src/test/java/nz/ac/waikato/its/dspace/citecounts/GetWOSCitecountsTest.java
@@ -1,6 +1,7 @@
 package nz.ac.waikato.its.dspace.citecounts;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.StringUtils;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
@@ -9,15 +10,28 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.xmlmatchers.namespace.SimpleNamespaceContext;
 
+import javax.xml.namespace.NamespaceContext;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 import static com.madgag.hamcrest.FileExistenceMatcher.exists;
-import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.isEmptyString;
+import static org.hamcrest.Matchers.*;
 import static org.xmlmatchers.XmlMatchers.isEquivalentTo;
 import static org.xmlmatchers.transform.XmlConverters.the;
+import static org.xmlmatchers.xpath.XpathReturnType.returningANumber;
 
 /**
  * @author Andrea Schweer schweer@waikato.ac.nz for the UoW Institutional Research Repositories
@@ -27,10 +41,38 @@ public class GetWOSCitecountsTest {
 		InputStream response = postXMLRequest(ClassLoader.getSystemResourceAsStream("citecounts/TestRequest.xml"), GetWOSCitecounts.WOS_SERVICE_URL);
 		assertThat(response, is(notNullValue()));
 
-		String responseStream = IOUtils.toString(response);
-		String storedResponse = IOUtils.toString(ClassLoader.getSystemResourceAsStream("citecounts/TestResponse.xml"));
+		DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+		documentBuilderFactory.setNamespaceAware(true);
+		DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+		Document document = documentBuilder.parse(response);
+		Node responseNode = document.getDocumentElement();
 
-		assertThat(the(responseStream), isEquivalentTo(the(storedResponse)));
+		NamespaceContext namespace = new SimpleNamespaceContext().withBinding("i", "http://www.isinet.com/xrpc42");
+
+		// check overall structure & response code are ok
+		assertThat(responseNode,
+				hasXPath("/i:response/i:fn[@name='LinksAMR.retrieve' and @rc='OK']",
+						namespace));
+
+		// check that there are responses for 50 handles
+		assertThat(responseNode,
+				hasXPath("count(/i:response/i:fn/i:map/i:map[starts-with(@name, '10289_')])",
+						namespace,
+						equalTo("50")));
+
+		// check that there is at least one response with the expected elements - sourceURL, timesCited and citingArticlesURL
+		assertThat(responseNode,
+				hasXPath("/i:response/i:fn/i:map/i:map[starts-with(@name, '10289_')]/i:map[@name='WOS']/i:val[@name='sourceURL']/text()",
+						namespace,
+						not(isEmptyString())));
+		assertThat(responseNode,
+				hasXPath("/i:response/i:fn/i:map/i:map[starts-with(@name, '10289_')]/i:map[@name='WOS']/i:val[@name='timesCited']/text()",
+						namespace,
+						not(isEmptyString())));
+		assertThat(responseNode,
+				hasXPath("/i:response/i:fn/i:map/i:map[starts-with(@name, '10289_')]/i:map[@name='WOS']/i:val[@name='citingArticlesURL']/text()",
+						namespace,
+						not(isEmptyString())));
 	}
 
 	private InputStream postXMLRequest(InputStream data, String serviceUrl) throws IOException {
@@ -51,6 +93,10 @@ public class GetWOSCitecountsTest {
 	}
 
 	@Test public void verifyConversion() throws Exception {
+		Date date = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		sdf.setTimeZone(TimeZone.getDefault());
+
 		InputStream response = ClassLoader.getSystemResourceAsStream("citecounts/TestResponse.xml");
 		File transformedResponse = File.createTempFile("transformed", "xml");
 		transformedResponse.deleteOnExit();
@@ -65,6 +111,9 @@ public class GetWOSCitecountsTest {
 		assertThat(generatedXml, not(isEmptyString()));
 
 		String storedTransformed = IOUtils.toString(ClassLoader.getSystemResourceAsStream("citecounts/TestResponseTransformed.xml"));
+
+		// update last updated date to today's date
+		storedTransformed = StringUtils.replaceOnce(storedTransformed, " last-updated=\"2016-10-11\"", " last-updated=\"" + sdf.format(date) + "\"");
 		assertThat(the(generatedXml), isEquivalentTo(the(storedTransformed)));
 
 		transformedResponse.delete();
